aa0408a26cab7ce27dd148970f556cf70dfaf605
Daniel Cuevas <danny89c33@gmail.com>
2020-04-20 20:33:58 -0700

    changed to keyup event

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const balloon = document.getElementById('balloon')
 
 const inflate = (event) => {
     if(event.keyCode === 73) {
-    style = window.getComputedStyle(balloon, null).getPropertyValue('font-size');
-    currentSize = parseFloat(style);
-    balloon.style.fontSize = (currentSize + 10) + 'px';
+        style = window.getComputedStyle(balloon, null).getPropertyValue('font-size');
+        currentSize = parseFloat(style);
+        balloon.style.fontSize = (currentSize + 10) + 'px';
 }
     
 }
@@ -18,5 +18,12 @@ const deflate = (event) => {
     
 }
 
-document.addEventListener('keydown', inflate)
-document.addEventListener('keydown', deflate)
+const pop = (event) => {
+    if(balloon.style.fontSize = "60px") {
+        balloon.innerHTML = "O";
+    }
+}
+
+document.addEventListener('keyup', inflate)
+document.addEventListener('keyup', deflate)
+document.removeEventListener('keyup', pop)
